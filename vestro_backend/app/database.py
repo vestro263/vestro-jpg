a7a591bb3a8f567519c38928cada980b41ef7cbd
icontroller-jpg <walternyika07@gmail.com>
2026-04-04 20:05:27 +0530

    gogogo

--- vestro_backend/app/database.py
+++ vestro_backend/app/database.py
@@ -1,4 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+import os
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+# Fix for render postgres URLs
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 class Base(DeclarativeBase):
     pass
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
