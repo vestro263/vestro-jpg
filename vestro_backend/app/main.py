6662da4006714c73bed750ddea30dcb8d82dc043
icontroller-jpg <walternyika07@gmail.com>
2026-04-05 23:00:57 +0530

    roti

--- vestro_backend/app/main.py
+++ vestro_backend/app/main.py
@@ -85,11 +85,12 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup")
 async def reset_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(reset_db())
 
 # ------------------ GLOBAL ERROR HANDLER ------------------
 @app.exception_handler(Exception)
